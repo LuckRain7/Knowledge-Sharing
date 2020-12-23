8b844e61be296f20bdc01f52d6da4bfe900e9daf
LuckRain7 <76383993@qq.com>
2020-12-23 20:52:34 +0800

    docs: update

--- uploading.sh
+++ uploading.sh
@@ -8,7 +8,7 @@ git add .
 git commit -m "docs: update"
 
 # Gitee
-git push git@gitee.com:LuckRain/Knowledge-Sharing.git master
+git push git@gitee.com:LuckRain7/Knowledge-Sharing.git master
 
 # GitHub
 git push git@github.com:LuckRain7/Knowledge-Sharing.git master
